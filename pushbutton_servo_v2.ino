5457c3510bbdac9be7851a9781883a7dccb2a44e
Victor-268 <victor.gevel@gmail.com>
2024-03-05 00:55:51 +0100

    ServoMotor

---
+++ pushbutton_servo_v2.ino
@@ -0,0 +1,25 @@
+//sketch created by Akshay Joseph follow me on Instagra: five_volt_player
+
+#include<Servo.h>
+Servo Myservo;
+int pos=0;
+
+void setup()
+{
+  pinMode(2,INPUT);
+  Myservo.attach(3);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if(digitalRead(2)==LOW){
+    Myservo.write(80);
+    Serial.println("x");
+   
+  }
+  else
+    
+ Myservo.write(0);
+
+}
